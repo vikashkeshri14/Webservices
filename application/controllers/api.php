29dd17ae4de3c85ed1a4d41d7dc99a39991bc4c1
vikashkeshri14 <34962193+vikashkeshri14@users.noreply.github.com>
2017-12-30 20:41:39 -0800

    changes

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -8,6 +8,8 @@ class Api extends CI_Controller {
     	 $this->load->helper('url');
 		 $this->load->library('session');
 		 $this->load->library('form_validation');	
+		 $this->load->model('model_test');
+		 $this->load->model('model_api');
 			//  $check=$this->check_valid();
 			$check=1;
 			  if($check=='false')
@@ -18,10 +20,22 @@ class Api extends CI_Controller {
 	  
 	  public function getService()
 	  {
-		  $this->load->model('model_test');
+		  
 		 echo json_encode($this->model_test->getService());
 	  }
-	  
+	  public function  registration()
+	  {
+		  if($this->input->post('username') && $this->input->post('email_id') && $this->input->post('phone_no') && $this->input->post('role_id'))
+		  {
+			  $this->model_api->registration();
+		  }
+		  else
+		  {
+			$data['request']="Error";
+			$data['message']="Check your request";
+			echo json_encode($data);  
+		  }
+	  }
 	  public function check_valid()
 	  {
 		 if($this->input->post('username')!='ApiUsername' && $this->input->post('ApiPassword')!='ApiPassword')
@@ -32,6 +46,20 @@ class Api extends CI_Controller {
 		 {
 			 return true;
 		 }
+	  }
+	  public function check_password()
+	  {
+		echo  $this->model_api->password_encrypt('vikash');
+	  }
+	   public function verify_password()
+	  {
+		echo  $this->model_api->password_verify();
+	  }
+	  public function get_rand()
+	  {
+		  echo $this->model_api->token();
+		  echo '<br>';
+		  echo $this->model_api->token();
 	  }
 		
 		
---
+++ application/controllers/apirequester.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Apirequester extends CI_Controller {
+		
+		
+public function __construct()
+	  {
+		  parent::__construct();
+    	 $this->load->helper('url');
+		 $this->load->library('session');
+		 $this->load->library('form_validation');	
+			//  $check=$this->check_valid();
+			$check=1;
+			  if($check=='false')
+			  {
+				  echo 'Check your url and api screate code before submiting';
+			  }
+	  }
+	  
+	  public function getService()
+	  {
+		  $this->load->model('model_test');
+		  echo '<pre>';
+		 echo json_encode($this->model_test->getService());
+	  }
+	  
+	  public function check_valid()
+	  {
+		 if($this->input->post('username')!='ApiUsername' && $this->input->post('ApiPassword')!='ApiPassword')
+		 {
+			 return true;
+		 }
+		 else
+		 {
+			 return true;
+		 }
+	  }
+		
+		
+		
+}
