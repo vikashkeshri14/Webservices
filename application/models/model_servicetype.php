9a50ba6a5fb707b6972e0ef5c46017e1dd08c5cf
MohammedFarhan <34986730+MohammedFarhan@users.noreply.github.com>
2018-01-08 20:28:31 +0300

    Service type get, get by id and delete
    New methods added.

--- application/models/model_servicetype.php
+++ application/models/model_servicetype.php
@@ -67,10 +67,72 @@ include('model_log.php');
 		}
 		catch(Exception $e)//catch exception
 		{
+			/**/
 			logwrite( 'model_servicetype.php >> updateservicetypes >> '.$e->getMessage());
 			return false;
 		}
 	}
+	
+ public function getservicetypes()//--Getting complete list of servicetypes available
+	{
+		 try
+		 {
+			$q = $this->db->get('servicetypes');
+			$data = $q->result_array();
+		 }
+		 catch(Exception $e)
+		 {
+			 /* Need to add the Logger class */
+			logwrite( 'model_servicetypes.php >> getservicetypes >> '.$e->getMessage());
+		 }
+		 
+	 }
+
+public function getservicetypesbyid()//-Get servicetypes by ID
+	{
+		 try
+		 {
+			if($this->input->post('hdid'))
+			{
+				$this->db->where('type_id', $id);
+				$q = $this->db->get('servicetypes');
+				$data = $q->result_array();
+			}
+			else
+			{
+				return false;
+			}
+		 }
+		 catch(Exception $e)
+		 {
+			 /* Need to add the Logger class */
+			logwrite( 'model_servicetypes.php >> getservicetypesbyid >> '.$e->getMessage());
+		 }		 
+	 }
+	 public function deleteservicetypes() //-Deleting the record from the table
+	 {
+		 try
+		 {
+			if($this->input->post('hdid'))
+			{
+				$id=$this->input->post('hdid');
+				/*--Deleting the record from Database..*/
+				$this->db->where('type_id', $id);
+				$del=$this->db->delete('servicetypes');   
+				return $del;
+			}
+			else
+			{
+				return false;
+			}
+		 }
+		catch(Exception $y)
+		 {
+			 /* Need to add the Logger class */
+			logwrite( 'model_servicetypes.php >> deleteservicetypes >> '.$y->getMessage());
+		 }
+	}
+
 }
 
 ?>
