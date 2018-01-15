b3cb9376619dea0e096039caa7ad67423e825efe
vikashkeshri14 <34962193+vikashkeshri14@users.noreply.github.com>
2018-01-14 19:53:01 -0800

    database

--- database/alpha.sql
+++ database/alpha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 31, 2017 at 03:44 AM
+-- Generation Time: Jan 15, 2018 at 04:51 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -38,6 +38,19 @@ CREATE TABLE `blocked_user` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cancel_service_token`
+--
+
+CREATE TABLE `cancel_service_token` (
+  `id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `token` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `city`
 --
 
@@ -45,6 +58,21 @@ CREATE TABLE `city` (
   `city_id` int(11) NOT NULL,
   `country_id` int(11) NOT NULL,
   `city_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created` timestamp NULL DEFAULT NULL,
+  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cms`
+--
+
+CREATE TABLE `cms` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
   `created` timestamp NULL DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -94,6 +122,17 @@ CREATE TABLE `email_token` (
   `status` enum('1','2') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=token not used, 2=token used'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `email_token`
+--
+
+INSERT INTO `email_token` (`email_token_id`, `user_id`, `email_token`, `created`, `status`) VALUES
+(2, 2, '4491', '2018-01-13 18:34:38', '1'),
+(5, 6, '7685', '2018-01-13 18:47:36', '1'),
+(6, 7, '8858', '2018-01-14 14:59:17', '1'),
+(7, 8, '6599', '2018-01-14 15:10:08', '1'),
+(8, 9, '9242', '2018-01-14 17:11:26', '1');
+
 -- --------------------------------------------------------
 
 --
@@ -107,6 +146,17 @@ CREATE TABLE `mobile_token` (
   `created` timestamp NULL DEFAULT NULL,
   `status` enum('1','2') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=token not used, 2=token used'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `mobile_token`
+--
+
+INSERT INTO `mobile_token` (`token_id`, `user_id`, `mobile_token`, `created`, `status`) VALUES
+(2, 2, '6429', '2018-01-13 18:34:38', '1'),
+(5, 6, '4946', '2018-01-13 18:47:36', '1'),
+(6, 7, '6371', '2018-01-14 14:59:17', '1'),
+(7, 8, '3375', '2018-01-14 15:10:08', '1'),
+(8, 9, '8486', '2018-01-14 17:11:26', '1');
 
 -- --------------------------------------------------------
 
@@ -135,9 +185,11 @@ CREATE TABLE `place_bids` (
   `user_id` int(11) NOT NULL,
   `bid_amount` float NOT NULL,
   `delivery` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `qualification` text COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `qualification` text COLLATE utf8_unicode_ci,
+  `description` text COLLATE utf8_unicode_ci,
   `status` enum('0','1') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '0=Disable,1=Enable',
+  `bid_accept` enum('0','1') COLLATE utf8_unicode_ci NOT NULL DEFAULT '0' COMMENT '0=Bid placed,1=Bid Accepted',
+  `token` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created` timestamp NULL DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -219,8 +271,10 @@ CREATE TABLE `service_request` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `service_types` int(11) NOT NULL,
   `expiry_date` datetime DEFAULT NULL,
-  `delivery_date` datetime DEFAULT NULL,
+  `delivery` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `skills` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `attachment` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('1','2') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1' COMMENT '1=enable, 2=disable',
   `created` timestamp NULL DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -229,8 +283,8 @@ CREATE TABLE `service_request` (
 -- Dumping data for table `service_request`
 --
 
-INSERT INTO `service_request` (`service_request_id`, `user_id`, `title`, `description`, `service_types`, `expiry_date`, `delivery_date`, `skills`, `created`, `updated`) VALUES
-(3, 1, 'test', 'sdfds', 1, '2017-12-28 00:00:00', '2017-12-31 00:00:00', 'rrrr', '2017-12-28 04:43:53', '2017-12-28 04:43:53');
+INSERT INTO `service_request` (`service_request_id`, `user_id`, `title`, `description`, `service_types`, `expiry_date`, `delivery`, `skills`, `attachment`, `status`, `created`, `updated`) VALUES
+(3, 1, 'test', 'sdfds', 1, '2017-12-28 00:00:00', '2017-12-31 00:00:00', 'rrrr', '', '1', '2017-12-28 04:43:53', '2017-12-28 04:43:53');
 
 -- --------------------------------------------------------
 
@@ -277,7 +331,7 @@ CREATE TABLE `user` (
   `role_id` int(11) NOT NULL,
   `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `gender` enum('1','2') COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '1=Male, 2=Female',
-  `user_token` text COLLATE utf8_unicode_ci NOT NULL,
+  `user_token` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'Mobile token id',
   `email_id` text COLLATE utf8_unicode_ci NOT NULL,
   `phone_no` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `iqama_id` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -288,7 +342,9 @@ CREATE TABLE `user` (
   `country` int(11) DEFAULT NULL,
   `city` int(11) DEFAULT NULL,
   `status` enum('0','1') COLLATE utf8_unicode_ci NOT NULL DEFAULT '0' COMMENT '0=Disable,1=Enable',
-  `created` datetime DEFAULT NULL,
+  `email_token_verify` enum('0','1') COLLATE utf8_unicode_ci NOT NULL DEFAULT '0' COMMENT '0=Token not verify,1=Token verify',
+  `phone_token_verify` enum('0','1') COLLATE utf8_unicode_ci NOT NULL DEFAULT '0' COMMENT '0=Token not verify,1=Token verify',
+  `created` timestamp NULL DEFAULT NULL,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -296,8 +352,13 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `role_id`, `username`, `gender`, `user_token`, `email_id`, `phone_no`, `iqama_id`, `address`, `user_status`, `comercial_registration`, `password`, `country`, `city`, `status`, `created`, `updated`) VALUES
-(1, 2, 'vikash', '1', 'jdsjfkjsd fsahfkjsndf snksd sdc smobile token', 'vikash@gmail.com', '98766463734', '236874533', 'test ihi swr\'xw\'e cr\'ew xew\r\n frewr;h ewew; h', '1', '3334', 'test', NULL, NULL, '0', '2017-12-27 00:00:00', '2017-12-28 04:42:17');
+INSERT INTO `user` (`user_id`, `role_id`, `username`, `gender`, `user_token`, `email_id`, `phone_no`, `iqama_id`, `address`, `user_status`, `comercial_registration`, `password`, `country`, `city`, `status`, `email_token_verify`, `phone_token_verify`, `created`, `updated`) VALUES
+(1, 2, 'vikash', '1', 'jdsjfkjsd fsahfkjsndf snksd sdc smobile token', 'vikash@gmail.com', '98766463734', '236874533', 'test ihi swr\'xw\'e cr\'ew xew\r\n frewr;h ewew; h', '1', '3334', '$2y$11$b1Kblxw5UH6xojHn09z9HONcs/IYpzegWhxjsK3vx61eWeeSwdAves', NULL, NULL, '0', '0', '0', '2017-12-27 08:00:00', '2018-01-03 06:27:43'),
+(2, 2, 'test', NULL, 'cyDS5uzfJus:APA91bGaTJfNlFtsfjPG_zuYfG--PdD4Fdp20IPVToCmrTmgFEfxPaQONGqiDVnZKRHeIdSebuxXt9KBqXgGwLEtbVwrsz2LrUvUJO1ILDE6L8WVngp_xJsNT0lwV_YATjFONjLSsAIj', 'test@gmail.com', '9876543675', NULL, NULL, '1', NULL, '$2y$11$4Hvowz73VhdnmVDfLxsSFe9YY.eV/o5wYmh5vbjdQau.XLxpbtTGW', NULL, NULL, '0', '0', '0', '2018-01-13 18:34:38', '2018-01-13 09:34:38'),
+(6, 2, 'tere', NULL, 'dzeNRo1iuPc:APA91bGmf4whmHKyDSmwgIDVFwr_Ga7TXzu881SOTJj_jmlRvy08A8tREmDXZviHqStE8F1Aa8fWkvS42LJOQjeIIacIi2iuNyYmTXU9nJaGqy-gC7pEQDu1MXPJPb9xV9AvaRVYxikQ', 'vkeshri.14@gmail.com', '3456789234', NULL, NULL, '1', NULL, '$2y$11$X8KN1CPh8oVX/IyGE5GRfOybhDs4gZM7DPSP9psts230qzRO6RfYC', NULL, NULL, '0', '0', '0', '2018-01-13 18:47:36', '2018-01-13 09:47:36'),
+(7, 2, 'test', NULL, 'dbmmLw4kgIs:APA91bGbgPQAgkYMHRC7eS99Tu7r6KI61ndJ_QTx3FojPOknS26kGkoPeMwKAqWtEzZRI5QuU3iHGFZEt5L3OujIZ4R6_7cqAcfxgOJYyU2yfuANhQEJogQiyFHqKS03wuS4xbminyYi', 'test@gmail.com', '7654876598', NULL, NULL, '1', NULL, '$2y$11$vQoOW5FVtqmRRxZZ/o/sW.YXwuk4OQTTn1WMLwc4Gk.3K/Zw8BoUi', NULL, NULL, '0', '0', '0', '2018-01-14 14:59:17', '2018-01-14 05:59:18'),
+(8, 2, 'test', NULL, 'c6UGOjrYwNQ:APA91bFpavg5rNcWN4VXeqQwEXC4R7-a8IC0T9azOy86x3ldvM1EHE1h3MOHRdh4B9_1o1emGD9h02ejetEoOxQLMrD4VWOBeGGG_LuGUAEahNDvuAWqowIy8TAGIv0t_bw1UA4EGNNl', 'test@gmail.com', '876543644', NULL, NULL, '1', NULL, '$2y$11$sKC2neA1arlFxkdB9xyc3e.TYyuPzCe9Gm0oc5ES9MmXh24ISLszW', NULL, NULL, '0', '0', '0', '2018-01-14 15:10:08', '2018-01-14 06:10:08'),
+(9, 2, 'test', NULL, 'ea65Telb7dk:APA91bGD4UpSZpnHBL19QoAr_3wW1iJ-ZEeBq_lYC8TfAXzeFE0mg1z61o6wRNT7MpGwCMKydIozJbR9ekxi0m0MSJ4Th5dvmGWmft6cTBHa1MRinsvLd6iZXhRUTQ-kvBEBpb6FP7Xe', 'test@gmail.com', '96784319433', NULL, NULL, '1', NULL, '$2y$11$EAHJWoLjHx5jgow81fPayuxo4fuXa6dWCglOmwQ1WxOliEIbL7S3G', NULL, NULL, '0', '0', '0', '2018-01-14 17:11:26', '2018-01-14 08:11:26');
 
 -- --------------------------------------------------------
 
@@ -349,11 +410,23 @@ ALTER TABLE `blocked_user`
   ADD KEY `blocked_id` (`blocked_id`);
 
 --
+-- Indexes for table `cancel_service_token`
+--
+ALTER TABLE `cancel_service_token`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `city`
 --
 ALTER TABLE `city`
   ADD PRIMARY KEY (`city_id`),
   ADD KEY `country_id` (`country_id`);
+
+--
+-- Indexes for table `cms`
+--
+ALTER TABLE `cms`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comment`
@@ -492,10 +565,22 @@ ALTER TABLE `blocked_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `cancel_service_token`
+--
+ALTER TABLE `cancel_service_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
   MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cms`
+--
+ALTER TABLE `cms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comment`
@@ -513,13 +598,13 @@ ALTER TABLE `country`
 -- AUTO_INCREMENT for table `email_token`
 --
 ALTER TABLE `email_token`
-  MODIFY `email_token_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `email_token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `mobile_token`
 --
 ALTER TABLE `mobile_token`
-  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `token_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `notification`
@@ -579,7 +664,7 @@ ALTER TABLE `spam_comment`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_certification`
