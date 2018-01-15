9510d342620bce4e44104e24776aeb9983152c07
vikashkeshri14 <34962193+vikashkeshri14@users.noreply.github.com>
2018-01-14 19:57:55 -0800

    changes

--- database/alpha.sql
+++ database/alpha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2018 at 04:51 AM
+-- Generation Time: Jan 15, 2018 at 04:56 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
